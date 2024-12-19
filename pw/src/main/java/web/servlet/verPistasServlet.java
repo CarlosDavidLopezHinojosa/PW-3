e0488f764b2ce26ad97f7fabe7e6a12e4ac59420
Alejandro Luque Núñez <152215355+alejandroluque04@users.noreply.github.com>
2024-12-19 20:50:50 +0100

    Update verPistasServlet.java

--- pw/src/main/java/web/servlet/verPistasServlet.java
+++ pw/src/main/java/web/servlet/verPistasServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import web.model.business.DTOs.PistaDTO;
-import web.model.data.DAOs.PistaDAO;
+import web.model.business.Gestores.GestorDePistas;
 
 @WebServlet("/verPistas")
 public class verPistasServlet extends HttpServlet {
@@ -17,11 +17,16 @@ public class verPistasServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            List<PistaDTO> pistas = PistaDAO.obtenerPistas();
+            // Obtener todas las pistas de la base de datos
+            List<PistaDTO> pistas = GestorDePistas.obtenerPistas();
+            // Establecer la lista de pistas como un atributo en el objeto request
             request.setAttribute("pistas", pistas);
+            // Reenviar la solicitud a la página JSP verPistas.jsp
             request.getRequestDispatcher("/views/verPistas.jsp").forward(request, response);
         } catch (Exception e) {
+            // Establecer un mensaje de error en caso de excepción
             request.setAttribute("mensaje", "Error al obtener las pistas: " + e.getMessage());
+            // Reenviar la solicitud a la página JSP verPistas.jsp
             request.getRequestDispatcher("/views/verPistas.jsp").forward(request, response);
         }
     }
